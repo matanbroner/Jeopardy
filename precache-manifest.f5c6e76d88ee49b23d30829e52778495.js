2853f6b01351b41cfd589b2d013578f98d803ade
Matan Broner <matanbroner@gmail.com>
2019-03-16 22:17:16 -0700

    Updates

---
+++ precache-manifest.f5c6e76d88ee49b23d30829e52778495.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1be9fcae46bd63c01c7c",
+    "url": "/Purim-Jeopardy/static/css/main.ea01d199.chunk.css"
+  },
+  {
+    "revision": "1be9fcae46bd63c01c7c",
+    "url": "/Purim-Jeopardy/static/js/main.0416becc.chunk.js"
+  },
+  {
+    "revision": "3b20500eeaa6b32df666",
+    "url": "/Purim-Jeopardy/static/js/runtime~main.4e3b4c17.js"
+  },
+  {
+    "revision": "216a05a0006ee2507b40",
+    "url": "/Purim-Jeopardy/static/js/2.f520679a.chunk.js"
+  },
+  {
+    "revision": "7866b640b7ffdbe5c90e7169cc9b6a32",
+    "url": "/Purim-Jeopardy/index.html"
+  }
+];
